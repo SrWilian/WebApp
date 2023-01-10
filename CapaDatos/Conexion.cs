49b5e1ddb721d884165ef2fb9d214395204364bb
ELVIS PAZ <Ewpc240@gmail.com>
2023-01-10 17:19:06 -0500

    Actualizar

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+
+    {
+        private static readonly Conexion _instancia = new Conexion();
+
+        public static Conexion Instancia
+        {
+            get { return _instancia; }
+        }
+
+        public SqlConnection Conectar()
+        {
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=DESKTOP-NM7EDQT;initial Catalog=Ecociudad;" + "Integrated Security=true";
+            return cn;
+
+        }
+    }
+}
