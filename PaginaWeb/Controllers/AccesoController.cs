a7a5548ccf844bd7a870b748488dff687a85837d
ELVIS PAZ <Ewpc240@gmail.com>
2023-01-27 13:45:03 -0500

    nav html

--- PaginaWeb/Controllers/AccesoController.cs
+++ PaginaWeb/Controllers/AccesoController.cs
@@ -34,6 +34,7 @@ namespace PRUEBAS_LOGIN.Controllers
         {
             return View();
         }
+         
 
         [HttpPost]
 
--- PaginaWeb/Controllers/HomeController.cs
+++ PaginaWeb/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 using PaginaWeb.Permisos;
 namespace PaginaWeb.Controllers
 {
-    [ValidarSesion]
+    //[ValidarSesion]
     public class HomeController : Controller
     {
         public ActionResult Index()
